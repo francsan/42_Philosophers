719d2a6e4dd1902a07406300d92b73b67b3481d7
Francisco Santos conceicao <francsan@c2r3s5.42lisboa.com>
2023-02-01 21:31:44 +0000

    creating rep and makefile

---
+++ philo/headers/philo.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: francsan <francsan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/01 20:26:32 by francsan          #+#    #+#             */
+/*   Updated: 2023/02/01 20:37:11 by francsan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+/* libraries */
+
+// memset
+# include <string.h>
+// printf
+# include <stdio.h>
+// malloc and free
+# include <stdlib.h>
+// write and usleep
+# include <unistd.h>
+// gettimeofday 
+# include <sys/time.h>
+// pthread_create, pthread_detach, pthread_join,
+// pthread_mutex_init, pthread_mutex_destroy,
+// pthread_mutex_lock, pthread_mutex_unlock
+# include <pthread.h>
+
+/* structs */
+
+/* sources */
+
+#endif
